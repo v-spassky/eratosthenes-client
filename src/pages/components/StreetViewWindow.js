5af730fe8132ce3b09f0c1b72baf90988fb6c19a
Vasiliy Spassky <spassky99@gmail.com>
2024-02-26 21:18:34 +0200

    Implement "leave room" btn and modal

--- src/pages/components/StreetViewWindow.js
+++ src/pages/components/StreetViewWindow.js
@@ -1,14 +1,49 @@
 import Map from './Map.js';
 import GoogleStreetView from '../../utils/maps/GoogleStreetView.js';
 import GoogleMapsWrapper from "../../utils/maps/GoogleMapsWrapper.js";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import { Button, Tooltip, Modal, ModalContent, ModalHeader, ModalFooter, useDisclosure } from "@nextui-org/react";
 
 function StreetViewWindow() {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const navigate = useNavigate();
+
+    const handleRoomExit = () => {
+        navigate("/");
+    }
+
     return (
         <div id="streetViewWindow" style={{ height: "100%", flexGrow: 1, position: "relative" }}>
             <GoogleMapsWrapper>
                 <GoogleStreetView />
                 <Map />
+                <Tooltip content="Покинуть комнату">
+                    <Button
+                        isIconOnly
+                        onClick={onOpen}
+                        color="primary"
+                        aria-label="Leave room"
+                        style={{ position: "absolute", bottom: "10px", left: "10px", zIndex: 1 }}
+                    >
+                        <FaArrowRightFromBracket />
+                    </Button>
+                </Tooltip>
             </GoogleMapsWrapper>
+
+            <Modal size={"sm"} isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader>Точно хочешь выйти из комнаты?</ModalHeader>
+                            <ModalFooter>
+                                <Button color="primary" onPress={onClose}>Нет...</Button>
+                                <Button color="danger" onPress={handleRoomExit}>Да, и поскорее!</Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </div>
     );
 }
