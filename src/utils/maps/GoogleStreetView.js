11a8c6934492c49b8194e8bf49b4f64a05a70287
Vasiliy Spassky <spassky99@gmail.com>
2024-03-04 22:59:25 +0200

    Use different default view position & move it into a file

--- src/utils/maps/GoogleStreetView.js
+++ src/utils/maps/GoogleStreetView.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-const DEFAULT_POSITION = { lat: 44.8566, lng: 10.3522 };
+import defaultStreetViewPosition from "../../constants/defaultStreetViewPosition.js";
 
 function GoogleStreetView() {
     const ref = useRef(null);
@@ -9,19 +9,14 @@ function GoogleStreetView() {
         if (ref.current) {
             new window.google.maps.StreetViewPanorama(ref.current, {
                 disableDefaultUI: true,
-                position: DEFAULT_POSITION,
+                position: defaultStreetViewPosition,
                 pov: { heading: 0, pitch: 0 },
                 zoom: 1,
             });
         }
     }, [ref]);
 
-    return (
-        <div
-            ref={ref}
-            style={{ width: "100%", height: "100%" }}
-        />
-    );
+    return <div ref={ref} style={{ width: "100%", height: "100%" }}/>;
 }
 
 export default GoogleStreetView;
