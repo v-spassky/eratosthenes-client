3d4d4a8e3a3f8e7358281e30df0211fc38f0f723
Vasiliy Spassky <spassky99@gmail.com>
2024-03-26 17:07:09 +0200

    Remove horizontal scroll in `UserList.js`

--- src/pages/components/UserList.js
+++ src/pages/components/UserList.js
@@ -1,5 +1,5 @@
 import {
-    Avatar, Badge, Chip, ScrollShadow, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow,
+    Avatar, Badge, Chip, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow,
 } from "@nextui-org/react";
 
 function medalOfIndex(index) {
@@ -17,44 +17,42 @@ function medalOfIndex(index) {
 
 export default function UserList({ users }) {
     return (
-        <div id="userList" style={{ height: "100%", flexGrow: 1, overflow: "auto" }}>
-            <ScrollShadow>
-                <Table removeWrapper hideHeader aria-label="List of users in the room">
-                    <TableHeader>
-                        <TableColumn>Avatar</TableColumn>
-                        <TableColumn>Name</TableColumn>
-                        <TableColumn>Score</TableColumn>
-                        <TableColumn>Description</TableColumn>
-                    </TableHeader>
-                    <TableBody>
-                        {users.map((user, index) => (
-                            <TableRow key={user.name}>
-                                {user.isHost
-                                    ? <TableCell style={{ fontWeight: "bold" }}>
-                                        <Badge content="host" color="danger" size="sm">
-                                            <Avatar name={user.avatarEmoji} style={{ fontSize: "35px" }} />
-                                        </Badge>
-                                    </TableCell>
-                                    : <TableCell style={{ fontWeight: "bold" }}>
+        <div id="userList" style={{ height: "100%", flexGrow: 1, overflowX: "hidden" }}>
+            <Table removeWrapper hideHeader aria-label="List of users in the room">
+                <TableHeader>
+                    <TableColumn>Avatar</TableColumn>
+                    <TableColumn>Name</TableColumn>
+                    <TableColumn>Score</TableColumn>
+                    <TableColumn>Description</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {users.map((user, index) => (
+                        <TableRow key={user.name}>
+                            {user.isHost
+                                ? <TableCell style={{ fontWeight: "bold" }}>
+                                    <Badge content="host" color="danger" size="sm">
                                         <Avatar name={user.avatarEmoji} style={{ fontSize: "35px" }} />
-                                    </TableCell>
-                                }
-                                <TableCell style={{ fontWeight: "bold", whiteSpace: "nowrap" }}>{user.name}</TableCell>
-                                <TableCell style={{ whiteSpace: "nowrap" }}>
-                                    <Chip style={{ background: "rgb(242, 244, 245)" }}>{user.score}</Chip>
-                                    <span style={{ marginLeft: "3px" }} ></span>
-                                    {medalOfIndex(index) !== ""
-                                        && <Chip style={{ background: "rgb(242, 244, 245)" }}>
-                                            {medalOfIndex(index)}
-                                        </Chip>
-                                    }
+                                    </Badge>
                                 </TableCell>
-                                <TableCell>{user.description}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </ScrollShadow>
+                                : <TableCell style={{ fontWeight: "bold" }}>
+                                    <Avatar name={user.avatarEmoji} style={{ fontSize: "35px" }} />
+                                </TableCell>
+                            }
+                            <TableCell style={{ fontWeight: "bold", whiteSpace: "nowrap" }}>{user.name}</TableCell>
+                            <TableCell style={{ whiteSpace: "nowrap" }}>
+                                <Chip style={{ background: "rgb(242, 244, 245)" }}>{user.score}</Chip>
+                                <span style={{ marginLeft: "3px" }} ></span>
+                                {medalOfIndex(index) !== ""
+                                    && <Chip style={{ background: "rgb(242, 244, 245)" }}>
+                                        {medalOfIndex(index)}
+                                    </Chip>
+                                }
+                            </TableCell>
+                            <TableCell>{user.description}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </div>
     );
 }
